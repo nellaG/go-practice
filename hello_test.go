e004f37530f4c24d4cb913abc2135c7d2a122d31
nellaG <seirios0107@gmail.com>
2024-01-26 15:36:34 +0900

    test helper

--- hello_test.go
+++ hello_test.go
@@ -7,16 +7,20 @@ func TestHello(t *testing.T) {
 		got := Hello("asdf")
 		want := "Hello, asdf"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
+
 	})
 	t.Run("say 'Hello, World' when string is empty", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
