d6d92d9a787fbb918a23c52133bf1777ecd17e01
nellaG <seirios0107@gmail.com>
2024-01-29 01:12:15 +0900

    switch case

--- hello.go
+++ hello.go
@@ -1,13 +1,21 @@
 package main
 
-const prefix = "Hello, "
+const englishPrefix = "Hello, "
+const spanish = "Spanish"
+const spanishPrefix = "Hola, "
+const french = "French"
+const frenchPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == "Spanish" {
-		return "Hola, " + name
+	prefix := englishPrefix
+	switch language {
+	case "French":
+		prefix = frenchPrefix
+	case "Spanish":
+		prefix = spanishPrefix
 	}
 	return prefix + name
 }
