e1b11957742250b72ea0352940c1bab3a5bdf7c2
nellaG <seirios0107@gmail.com>
2024-01-29 03:14:26 +0900

    add exmple

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	// t.Fatal("not implemented")
@@ -16,4 +19,10 @@ func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a")
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("b")
+	fmt.Println(repeated)
+	// output: bbbbb
 }
