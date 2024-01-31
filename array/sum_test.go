caeb6bf997141fd4d73a7c812f2f9195c5063a9e
nellaG <seirios0107@gmail.com>
2024-01-31 16:29:34 +0900

    loop and sum

--- array/sum_test.go
+++ array/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -29,7 +32,8 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
-	if got != want {
+	// not type safe
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
