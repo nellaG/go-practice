5cc334a535c19d4d00115dcf05beece777142fdd
nellaG <seirios0107@gmail.com>
2024-03-02 19:43:09 +0900

    add basic tests

--- concurrency/check_websites_test.go
+++ concurrency/check_websites_test.go
@@ -1,7 +1,32 @@
 package concurrency
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T) {
-	// t.Fatal("not implemented")
+func mockWebsiteChecker(url string) bool {
+	if url == "some://mock.urls" {
+		return false
+	}
+	return true
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"some://mock.urls",
+	}
+
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"some://mock.urls":           false,
+	}
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("wanted %v, got %v", want, got)
+	}
 }
