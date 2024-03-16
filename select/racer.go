9e08459d62a9bdf7ea263f4368775eed35357646
nellaG <seirios0107@gmail.com>
2024-03-16 19:27:57 +0900

    use select for multiple channels

--- select/racer.go
+++ select/racer.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"net/http"
-	"time"
 )
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func Racer(a, b string) (winner string) {
+	select {
+	case <-ping(a):
+		return a
+	case <-ping(b):
+		return b
+	}
 }
 
-func Racer(a, b string) (winner string) {
-
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-	if aDuration < bDuration {
-		return a
-	}
-	return b
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
