f6a8b0dac5a56e18910d8177e84c8bcdf6a97273
nellaG <seirios0107@gmail.com>
2024-03-16 18:25:42 +0900

    make http server for test

---
+++ select/racer_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+}
+
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayedServer(20)
+	fastServer := makeDelayedServer(0)
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+	slowServer.Close()
+	fastServer.Close()
+}
