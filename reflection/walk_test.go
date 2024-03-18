7462ff38edaec00e0ec58fa839fa8935212c966e
nellaG <seirios0107@gmail.com>
2024-03-18 17:00:36 +0900

    basic reflection

---
+++ reflection/walk_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"struct with two string field",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+			fmt.Printf("%s", got)
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %q, want %q", got[0], test.ExpectedCalls)
+			}
+		})
+	}
+}
